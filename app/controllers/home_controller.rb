a939d986ecfc177aff86d8ec8998ea9832587c07
“JIHYUN” <791004_@likelion.org>
2018-08-19 17:31:05 +0900

    jihyun

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,47 @@
+class HomeController < ApplicationController
+  require 'open-uri'
+  require 'json'
+  require 'cgi'
+
+  def landing
+  end
+
+  def index
+  end
+
+  def search_around
+    results = get_infos(params[:search])
+    respond_to do |format|
+      format.json {
+        render json: results
+      }
+    end
+  end
+
+  def search_center
+    encoded = CGI::escape(params[:search])
+    data = JSON.load(open("https://openapi.naver.com/v1/search/local.json?query=#{encoded}&start=1&display=1",
+    "X-Naver-Client-Secret" => "NteRqJBSgR",
+    "X-Naver-Client-Id" => "hoAJkV1ejgYLcx1aMVWu"))
+    
+    respond_to do |format|
+      format.json {
+        render json: data["items"][0]
+      }
+    end
+  end
+
+  private
+   def get_infos(query)
+    keywords = ["gs25","cu","미니스톱","세븐일레븐","올리브영","약국","성인용품","왓슨스","롭스"]
+    results = {}
+    keywords.each do |keyword|
+      encodedQuery = CGI::escape(query + " 근처 " + keyword)
+      data = JSON.load(open("https://openapi.naver.com/v1/search/local.json?query=#{encodedQuery}&start=1&display=50",
+      "X-Naver-Client-Secret" => "NteRqJBSgR",
+      "X-Naver-Client-Id" => "hoAJkV1ejgYLcx1aMVWu"))["items"]
+      results[keyword] = data
+    end
+    return results
+   end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root 'home#landing'
+  get '/find' => 'home#index'
+  get '/search-around/:search' => "home#search_around"
+  get '/search-center/:search' => "home#search_center"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
